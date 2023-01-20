5ff8a58f95d023a537be0bd55bb8c482c6329db2
Andrzej Popiolek <xandrzejpopiolek@gmail.com>
2023-01-20 20:40:12 +0100

    Init commit

---
+++ client/app.js
@@ -0,0 +1,47 @@
+const loginForm = document.getElementById('welcome-form');
+const messagesSection = document.getElementById('messages-section');
+const messagesList = document.getElementById('messages-list');
+const addMessageForm = document.getElementById('add-messages-form');
+const userNameInput = document.getElementById('username');
+const messageContentInput = document.getElementById('message-content');
+
+let userName = '';
+
+const login = (e) => {
+  e.preventDefault();
+  console.log('xd');
+  if (!userNameInput.value) {
+    alert('User Name must not be empty');
+  } else {
+    userName = userNameInput.value;
+    loginForm.classList.remove('show');
+    messagesSection.classList.add('show');
+  }
+};
+
+const addMessage = (author, content) => {
+  const message = document.createElement('li');
+  message.classList.add('message', 'message--received');
+  if (author === userName) {
+    message.classList.add('message--self');
+  }
+  message.innerHTML = ` <h3 class="message__author">${
+    author === userName ? 'You' : author
+  }</h3>
+    <div class="message__content">${content}</div>
+  </li>`;
+  messagesList.appendChild(message);
+};
+
+const sendMessage = (e) => {
+  e.preventDefault();
+  if (!messageContentInput.value) {
+    alert('You must write the message');
+  } else {
+    addMessage(userName, messageContentInput.value);
+    messageContentInput.value = '';
+  }
+};
+
+loginForm.addEventListener('submit', login);
+addMessageForm.addEventListener('submit', sendMessage);
